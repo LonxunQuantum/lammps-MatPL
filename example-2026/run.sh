0a48e6d44a8cc3722948c7a1f159f691b3546d5e
Yazhuo_Liu_z840 <liuyazhuo99@gmail.com>
2026-04-04 03:19:26 -0400

    fix example generaliaty

--- example-2026/run.sh
+++ example-2026/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run the simulation
-mpirun -n 2 /tools/yazhuoSoftware/NEP-lammps/lammps2026/build/lmp -k on g 4 -sf kk -pk kokkos newton on neigh half gpu/aware off -in HfO2.in
+mpirun -n 2 lmp -k on g 2 -sf kk -pk kokkos newton on neigh half gpu/aware off -in HfO2.in
