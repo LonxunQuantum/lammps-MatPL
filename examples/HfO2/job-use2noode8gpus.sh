5f87dcdef69fc5abf8c862442f9b07621bab2745
pfsuo <pfsuo@whu.edu.cn>
2026-06-04 14:10:30 +0800

    NPROCS fixed

--- examples/HfO2/job-use2noode8gpus.sh
+++ examples/HfO2/job-use2noode8gpus.sh
@@ -16,7 +16,7 @@ export OMPI_MCA_btl_openib_allow_ib=1
 export OMP_NUM_THREADS=1 
 
 # use 2node with 8 gpus
-mpirun -np $SLURM_NPORCS --bind-to numa --map-by ppr:4:node lmp -k on g 4 -sf kk -pk kokkos -in kkin.lmp
+mpirun -np $SLURM_NPROCS --bind-to numa --map-by ppr:4:node lmp -k on g 4 -sf kk -pk kokkos -in kkin.lmp
 
 # use 1node with 4gpus
 # mpirun -np 4 --bind-to numa --map-by ppr:4:node lmp -k on g 4 -sf kk -pk kokkos -in kkin.lmp
